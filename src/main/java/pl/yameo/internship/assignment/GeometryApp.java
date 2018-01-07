b7e3064084e08ef88b0961296dbbc27585e67063
Michał Niskowski <niski315@gmail.com>
2018-01-07 21:28:05 +0100

    add final refactor in GeometryApp (i guess)

--- src/main/java/pl/yameo/internship/assignment/GeometryApp.java
+++ src/main/java/pl/yameo/internship/assignment/GeometryApp.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GeometryApp {
+
 	private Reader reader;
 	private List<Shape> shapes = new ArrayList<>();
 
@@ -12,6 +13,7 @@ public class GeometryApp {
 	}
 
 	public void start() {
+
 		boolean run = true;
 		while (run) {
 			run = run();
@@ -19,6 +21,7 @@ public class GeometryApp {
 	}
 
 	private boolean run() {
+
 		System.out.println("Choose action:");
 		System.out.println("1) Create new shape");
 		System.out.println("2) List existing shapes");
@@ -26,6 +29,7 @@ public class GeometryApp {
 		System.out.println("0) Exit");
 
 		int option = reader.readInteger();
+
 		if (option == 0) {
 			return false;
 		} else if (option == 1) {
@@ -43,6 +47,7 @@ public class GeometryApp {
 	}
 
 	private Shape createNewShape() {
+
 		System.out.println("Choose shape to create:");
 		System.out.println("1) Ellipse");
 		System.out.println("2) Rectangle");
@@ -51,6 +56,7 @@ public class GeometryApp {
 		System.out.println("5) Triangle");
 		System.out.println("6) Rhombus");
 		System.out.println("0) Back");
+
 		int option = reader.readInteger();
 
 	 	if (option == 1) {
@@ -71,23 +77,29 @@ public class GeometryApp {
 	}
 
 	private void listShapes() {
+
 		System.out.println("====== LIST OF SHAPES ======");
+
 		shapes.forEach(shape -> {
 			System.out.print(shape.getName() + " with dimensions: ");
 			System.out.print(shape.listDimensions() + "; ");
 			System.out.print("Area: " + shape.calculateArea() + "; ");
 			System.out.println("Perimeter: " + shape.calculatePerimeter());
 		});
+
 		System.out.println("============================");
 	}
 
 	private void modifyShape() {
+
 		listShapes();
 
 		System.out.println("Please choose the index of the shape you want to modify (1-" + shapes.size() + "): ");
 
 		int index = reader.readInteger();
+
 		Shape activeShape = shapes.get(index - 1);
+
 		List<Double> oldDimensions = activeShape.listDimensions();
 		Double oldPerimeter = activeShape.calculatePerimeter();
 		Double oldArea = activeShape.calculateArea();
@@ -98,30 +110,36 @@ public class GeometryApp {
 		System.out.println("Perimeter: " + oldPerimeter);
 
 		if (activeShape instanceof Ellipse) {
+
 			System.out.println("Please provide two semi-axis lengths (major, minor):");
 			((Ellipse) activeShape).setSemiMajorAxis(reader.readDouble());
 			((Ellipse) activeShape).setSemiMinorAxis(reader.readDouble());
 
 		} else if (activeShape instanceof Circle) {
+
 			System.out.println("Please provide the radius for the circle:");
 			((Circle) activeShape).setRadius(reader.readDouble());
 
 		} else if (activeShape instanceof Square) {
+
 			System.out.println("Please provide the edge length:");
 			((Square) activeShape).setDimension(reader.readDouble());
 
 		} else if (activeShape instanceof Rectangle) {
+
 			System.out.println("Please provide two edge lengths (height, width):");
 			((Rectangle) activeShape).setHeight(reader.readDouble());
 			((Rectangle) activeShape).setWidth(reader.readDouble());
 
 		} else if (activeShape instanceof Triangle) {
+
 			System.out.println("Please provide three edge lengths:");
 			((Triangle) activeShape).setEdgeA(reader.readDouble());
 			((Triangle) activeShape).setEdgeB(reader.readDouble());
 			((Triangle) activeShape).setEdgeC(reader.readDouble());
 		}
 		else if (activeShape instanceof Rhombus) {
+
 			System.out.println("Please provide diagonals lengths(major, minor):");
 			((Rhombus) activeShape).setMajorDiagonal(reader.readDouble());
 			((Rhombus) activeShape).setMinorDiagonal(reader.readDouble());
@@ -143,31 +161,37 @@ public class GeometryApp {
 	}
 
 	private Ellipse createNewEllipse() {
+
 		System.out.println("Please provide two semi-axis lengths (major, minor):");
 		return new Ellipse(reader.readDouble(), reader.readDouble());
 	}
 
 	private Rectangle createNewRectangle() {
+
 		System.out.println("Please provide two edge lengths (height, width):");
 		return new Rectangle(reader.readDouble(), reader.readDouble());
 	}
 
 	private Circle createNewCircle() {
+
 		System.out.println("Please provide the radius for the circle:");
 		return new Circle(reader.readDouble());
 	}
 
 	private Square createNewSquare() {
+
 		System.out.println("Please provide the edge length:");
 		return new Square(reader.readDouble());
 	}
 
 	private Triangle createNewTriangle() {
+
 		System.out.println("Please provide three edge lengths:");
 		return new Triangle(reader.readDouble(), reader.readDouble(), reader.readDouble());
 	}
 
 	private Rhombus createNewRhombus() {
+
 		System.out.println("Please provide diagonals lengths(major, minor):");
 		return new Rhombus(reader.readDouble(), reader.readDouble());
 	}
